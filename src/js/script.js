76359f7430bed6b038f2999eecabf776f21462a0
evka07 <138365894+evka07@users.noreply.github.com>
2023-08-28 11:47:21 +0200

    sending form

--- src/js/script.js
+++ src/js/script.js
@@ -68,6 +68,11 @@
     };
 
     const settings = {
+        db: {
+            url: '//localhost:3131',
+            products: 'products',
+            orders: 'orders',
+        },
         amountWidget: {
             defaultValue: 1,
             defaultMin: 1,
@@ -519,18 +524,65 @@
 
     const app = {
         initMenu: function () {
-            for (const productData in dataSource.products) {
-                new Product(productData, dataSource.products[productData]);
+            const thisApp = this;
+
+            for (const productData in thisApp.data.products) {
+                console.log('Product ID:', productData);
+                console.log('Product Data:', thisApp.data.products[productData]);
+
+                new Product(productData, thisApp.data.products[productData]);
             }
+            thisApp.initAccordion();
         },
         initData: function () {
             const thisApp = this;
-            thisApp.data = dataSource;
+            thisApp.data = {};
+
+            const url = settings.db.url + '/' + settings.db.products;
+
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    thisApp.data.products = data;
+                    thisApp.initMenu();
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
         },
         initCart: function () {
             const thisApp = this;
             const cartElement = document.querySelector(select.containerOf.cart);
             thisApp.cart = new Cart(cartElement);
+
+            const orderForm  = cartElement.querySelector(select.cart.form)
+            orderForm.addEventListener('submit', function (event){
+                event.preventDefault()
+
+                const formData = new FormData(orderForm)
+                const orderData = {
+                    phone: formData.get('phone'),
+                    address: formData.get('address'),
+                    products:thisApp.cart.products.map(product => ({
+                        id: product.id,
+                        amount: product.amount
+                    }))
+                }
+                fetch(`${settings.db.url}/${settings.db.orders}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(orderData)
+                })
+                    .then(response => response.data)
+                    .then(data => {
+                        console.log('Ok', data)
+                    })
+                    .catch(error => {
+                        console.error("Error", error)
+                    })
+            })
         },
         init: function () {
             const thisApp = this;
@@ -544,6 +596,7 @@
 
             for (const header of productHeaders) {
                 header.addEventListener('click', function () {
+                    console.log('Product header clicked:', header);
                     const productElement = this.parentElement;
                     const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
 
@@ -557,6 +610,8 @@
                 });
             }
         },
+
+
     };
 
     app.init();
